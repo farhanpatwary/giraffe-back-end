503e74295310ac9a44f4b0e4b7ae6dbfd919b5ae
farhan <patwary.farhan87@gmail.com>
2019-11-11 16:24:10 +0000

    checking for connection string

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const app = require('./app')
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
 	console.log(`Listening on port: ${port}`);
+	console.log(`URL ${process.env.DB_URL}`)
 })
