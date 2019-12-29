b9c6398a623a3357771471deca1f74cde398c408
farhan <patwary.farhan87@gmail.com>
2019-12-29 13:33:39 +0000

    added server status route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('./app')
 const port = process.env.PORT || 8000;
+app.get('/status', (req,res)=>{
+	res.status(200).send('Server Online')
+})
 app.listen(port, () => {
 	console.log(`Listening on port: ${port}`);
-	console.log(`URL ${process.env.DB_URL}`)
 })
